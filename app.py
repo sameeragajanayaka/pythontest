6f1fa13ec33896d807151dd9409cdd9b21ec5987
sameera gajanayaka <48169938+sameera98@users.noreply.github.com>
2021-08-25 12:23:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,jsonify,request
+from nrclex import NRCLex as ns
+import nltk
+nltk.download('punkt')
+from textblob import TextBlob
+
+app=Flask(__name__)
+
+
+@app.route('/api')
+def get():
+    d={}
+    result=str(request.args['Query'])
+    emotion = ns(result)
+    analysis = TextBlob(result);
+    d={}
+    m={}
+    m['fear']=str(emotion.affect_frequencies['fear'])
+    m['anger']=str(emotion.affect_frequencies['anger'])
+    m['anticip']=str(emotion.affect_frequencies['anticip'])
+    m['trust']=str(emotion.affect_frequencies['trust'])
+    m['surprise']=str(emotion.affect_frequencies['surprise'])
+    m['sadness']=str(emotion.affect_frequencies['sadness'])
+    m['disgust']=str(emotion.affect_frequencies['disgust'])
+    m['joy']=str(emotion.affect_frequencies['joy'])
+    d['Query']=str(m)
+    d['Senti']=str(analysis.polarity)+","+str(analysis.subjectivity)
+    return jsonify(d)
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # print_hi('love')
+    app.run()
